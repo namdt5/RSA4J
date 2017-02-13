b022fbf5b447b6109b54ba125454b6c9b7445d6b
namdt55555 <thanhnam1609@gmail.com>
2017-02-13 13:12:29 +0700

    Update Rsa4j.java

--- Rsa4j.java
+++ Rsa4j.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.tbsolution.util;
+package com.test.util;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
